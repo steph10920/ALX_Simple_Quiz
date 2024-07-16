44e5b1bb91293a607bc77a61756521bd7cf8ec58
steph10920 <123088349+steph10920@users.noreply.github.com>
2024-07-16 17:59:20 +0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,16 +1,17 @@
-function checkAnswer(){
-    //identify the correct answer
-    var correctAnswer="4";
-    //retrieve the User's Answer
-    var userAnswer=document.querySelector('input[name="quiz"]:checked').value;
-    //Compare the User’s Answer with the Correct Answer:
+function checkAnswer() {
+    // Identify the Correct Answer
+    var correctAnswer = "4";
 
-    if (userAnswer===correctAnswer){
-        document.getElementById("feedback").textContent="Correct! Well done.";
+    // Retrieve the User’s Answer
+    var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    }else{
-        document.getElementById("feedback").textContent="That's incorrect. Try again!";
+    // Compare the User’s Answer with the Correct Answer
+    if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done.";
+    } else {
+        document.getElementById("feedback").textContent = "That's incorrect. Try again!";
     }
 }
-//Add an Event Listener to the Submit Button:
+
+// Add an Event Listener to the Submit Button
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
