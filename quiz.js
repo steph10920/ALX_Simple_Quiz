302d71d1da946ba933ca6a6c256bad3dde90db4d
steph10920 <123088349+steph10920@users.noreply.github.com>
2024-07-16 17:46:25 +0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -5,10 +5,10 @@ function checkAnswer(){
     var userAnswer=document.querySelector('input[name="quiz"]:checked').value;
     //Compare the User’s Answer with the Correct Answer:
     if(userAnswer===correctAnswer){
-        document.getElementById("feedback").textContent="correct! well done";
+        document.getElementById("feedback").textContent="Correct! Well done.";
 
     }else{
-        document.getElementById("feedback").textContent="Thats incorrect. Try again!";
+        document.getElementById("feedback").textContent="That's incorrect. Try again!";
     }
 }
 //Add an Event Listener to the Submit Button:
